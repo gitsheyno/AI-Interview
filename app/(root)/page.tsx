23b69c0bb04967760257ef7005742b7d47a7710e
shayan heidary <hdrydeveloper@gmail.com>
2025-04-14 20:13:19 +0200

    ad navbar and homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,40 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview ready with AI power</h2>
+          <p className="text-lg">
+            Practsie your interviews with your AI assistant. Get instant
+            feedback
+          </p>
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Strart an Interview</Link>
+          </Button>
+        </div>
+        <Image
+          src="/robot.png"
+          alt="robot"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+      <section className="flex- flex-col gap-6 mt-8">
+        <h2>Your Interviews</h2>
+        <div className="interviews-section">
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+      <section className="flex flex-col gap-6 mt-8">
+        <h2>Take an Interview</h2>
+        <div className="interview-section">
+          <p>there are no interviews available</p>
+        </div>
+      </section>
+    </>
+  );
 }
