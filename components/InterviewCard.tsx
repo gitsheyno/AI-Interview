2821922c62eacb74620a4796dd99b112f2d86946
shayan heidary <hdrydeveloper@gmail.com>
2025-04-16 17:50:38 +0200

    complete homepage - interviewCard

---
+++ components/InterviewCard.tsx
@@ -0,0 +1,70 @@
+"use client";
+import dayjs from "dayjs";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import DisplayTechIcons from "./DisplayTechIcons";
+
+const InterviewCard = ({
+  interviewId,
+  userId,
+  role,
+  type,
+  techstack,
+  createdAt,
+}: InterviewCardProps) => {
+  const feedback = null as Feedback | null;
+  const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
+  const formatedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MMM/D/YYYY");
+
+  return (
+    <div className="card-border w-[460px] max-sm:w-full min-h-96">
+      <div className="card-interview">
+        <div>
+          <div className="absolute top-0 right-0 w-fit px-4 py-2 rounded-bl-lg bg-light-600">
+            <p className="badge-text">{normalizedType}</p>
+          </div>
+          <Image
+            src="/covers/adobe.png"
+            alt="cover"
+            width={90}
+            height={90}
+            className="roudned-full object-fit size-[90px]"
+          />
+          <h3 className="mt-5 capitalize">{role} Interview</h3>
+          <div className="flex felx-row gap-5 mt-3">
+            <div className="flex flex-row gap-2">
+              <Image src="/calendar.svg" alt="cover" width={22} height={22} />
+              <p>{formatedDate}</p>
+              <div className="flex flex-row gap-2 items-center">
+                <Image src="/star.svg" alt="cov" width={22} height={22} />
+                <p>{feedback?.totalScore || "---"}/100</p>
+              </div>
+            </div>
+          </div>
+          <p className="line-clamp-2 mt-5">
+            {feedback?.finalAssessment || "you have not taken interview"}
+          </p>
+        </div>
+        <div className="flex flex-row justify-between">
+          <DisplayTechIcons techStack={techstack} />
+          <Button className="btn-primary">
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? "View Feedback" : "View Interview"}
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InterviewCard;
