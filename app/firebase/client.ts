a03c32a1da1a09498e3fe8dea76ff000ea27a549
shayan heidary <hdrydeveloper@gmail.com>
2025-04-19 18:44:06 +0200

    env

--- app/firebase/client.ts
+++ app/firebase/client.ts
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { env } from "process";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBoMdSoRA8ln3uNMfswTDbasCjydE7ywrw",
+  apiKey: process.env.API_KEY,
   authDomain: "ai-intevriew.firebaseapp.com",
   projectId: "ai-intevriew",
   storageBucket: "ai-intevriew.firebasestorage.app",
@@ -18,4 +19,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
