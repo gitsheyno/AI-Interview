4c8e12518eeb8c95f7e63e59f8be26427233b1b0
shayan heidary <hdrydeveloper@gmail.com>
2025-04-19 18:15:38 +0200

    server sdk

---
+++ app/firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBoMdSoRA8ln3uNMfswTDbasCjydE7ywrw",
+  authDomain: "ai-intevriew.firebaseapp.com",
+  projectId: "ai-intevriew",
+  storageBucket: "ai-intevriew.firebasestorage.app",
+  messagingSenderId: "391122074547",
+  appId: "1:391122074547:web:f0085608862e49de1a69c6",
+  measurementId: "G-SK22ELYKSV",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
