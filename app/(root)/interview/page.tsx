4b214e54076bc7a20a2daf8799271f7e5ef251c8
shayan heidary <hdrydeveloper@gmail.com>
2025-04-21 14:25:53 +0200

    add interview agent component

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,11 @@
+import Agent from "@/components/Agent";
+const InterviewPage = () => {
+  return (
+    <>
+      <h3>Interview genertion</h3>
+      <Agent userName="shayan" userId="user1" type="generate" />
+    </>
+  );
+};
+
+export default InterviewPage;
