5fedc9cf783683aefaec930bb9856e3c76595601
shayan heidary <hdrydeveloper@gmail.com>
2025-05-05 11:38:37 +0200

    add get latest interviews and optimized data fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,13 +6,21 @@ import InterviewCard from "@/components/InterviewCard";
 import {
   getCurrentUser,
   getInterviewByUserId,
+  getLatestInterviews,
 } from "@/lib/actions/auth.action";
 export default async function Home() {
   const user = await getCurrentUser();
-  const userInterviews = await getInterviewByUserId(user?.id as string);
+
+  const [userInterviews, latestInterviews] = await Promise.all([
+    await getInterviewByUserId(user?.id as string),
+    await getLatestInterviews({
+      userId: user?.id as string,
+    }),
+  ]);
 
   const hasPastIntervoews = userInterviews && userInterviews?.length > 0;
-
+  const hasUpCommingInterviews =
+    latestInterviews && latestInterviews?.length > 0;
   return (
     <>
       <section className="card-cta">
@@ -49,9 +57,13 @@ export default async function Home() {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Take an Interview</h2>
         <div className="interviews-section">
-          {dummyInterviews.map((interview) => (
-            <InterviewCard key={interview.id} {...interview} />
-          ))}
+          {hasUpCommingInterviews ? (
+            latestInterviews.map((interview) => (
+              <InterviewCard key={interview.id} {...interview} />
+            ))
+          ) : (
+            <p>You haven&apos;t taken any interviews yet</p>
+          )}
         </div>
       </section>
     </>
--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -139,3 +139,20 @@ export async function getInterviewByUserId(
     ...doc.data(),
   })) as Interview[];
 }
+
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+  const interviews = await db
+    .collection("interviews")
+    .where("finalized", "==", true)
+    .where("userId", "!=", userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
